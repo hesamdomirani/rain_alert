4b7007c592c47a4fcbc268806a66a119223b14ec
hesam_domirani <domiranihesam@gmail.com>
2024-11-10 18:55:14 +0100

    Send a Message when it's Rainy

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import requests
 import os
 from dotenv import load_dotenv
+from twilio.rest import Client
 
 load_dotenv()
 api_key = os.getenv('API_KEY')
+auth_token = os.getenv("AuthToken")
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 LATITUDE = 50.316540
@@ -21,8 +23,16 @@ weather_for_12_hours = weather_data_total["list"]
 code_status = [item["weather"][0]["id"] for item in weather_for_12_hours]
 will_rain = False
 for code in code_status:
-    if code < 700:
+    if code < 900:
         will_rain=True
 if will_rain:
-    print("Bring your Umbrella!!")
-      
+    account_sid = 'ACa391b5b2cdb03d246bac190be0e60e7a'
+    auth_token = auth_token
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+    from_='+12517327108',
+    body='It will be Rainy,Bring Your Umbrella!!!',
+    to='+491783997382'
+)
+    print(message.sid)
